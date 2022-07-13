6b7ffa098996b53de8156aa7391849a071ef196a
Isaac Byrne <me@ipbyrne.com>
2022-07-13 14:49:01 -0500

    init

---
+++ pages/chapi/set.js
@@ -0,0 +1,50 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+
+const ChapiSet = () => {
+  const [chapiState, setChapiState] = useState({});
+  useEffect(() => {
+    (async () => {
+      const { WebCredentialHandler } = window;
+      const event = await WebCredentialHandler.receiveCredentialEvent();
+      setChapiState({ event });
+    })();
+  }, []);
+  const handleStoreCredential = () => {
+    const {
+      event: {
+        credential: { data },
+      },
+    } = chapiState;
+    console.log("wallet stored data ", data);
+    // TODO: save local storage
+    chapiState.event.respondWith(
+      new Promise((resolve) => {
+        return data
+          ? resolve({ dataType: "VerifiablePresentation", data })
+          : resolve(null);
+      })
+    );
+  };
+  return (
+    <>
+      <Head>
+        <title>{"CHAPI SET"}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script src="https://unpkg.com/credential-handler-polyfill@2.1.0/dist/credential-handler-polyfill.min.js"></script>
+        <script src="https://unpkg.com/web-credential-handler@2.0.0/dist/web-credential-handler.min.js"></script>
+      </Head>
+      <div>
+        <div sx={{ mt: 8 }}>
+          <button onClick={handleStoreCredential}>
+            Save Credentials
+          </button>
+          <pre>{JSON.stringify(chapiState, null, 2)}</pre>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ChapiSet;
